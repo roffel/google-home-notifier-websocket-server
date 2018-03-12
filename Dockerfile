aba21c15dc9fcea4bfd468d3824e4cce41b08d1d
Roffel <r.roffel@st.hanze.nl>
2018-03-12 19:34:12 +0100

    Added GET option

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM node:latest
 RUN apt-get update
 RUN apt-get install git-core libnss-mdns libavahi-compat-libdnssd-dev -yy
 WORKDIR /usr/src/app
-COPY . .
 COPY package*.json ./
-
 RUN npm install --save
+COPY . .
--- app.js
+++ app.js
@@ -7,24 +7,40 @@ const PORT = 8080;
 
 const WebSocket = require('ws');
 var googlehome = require('google-home-notifier');
-var language = 'nl'; // if not set 'us' language will be used
+const language = 'nl'; // if not set 'us' language will be used
+var app = express();
 
 const wss = new WebSocket.Server({ port: PORT });
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
-        googlehome.ip('192.168.1.85', language);
-        if (message.startsWith('http')){
-            // @TODO: Add more checks.
-            // You can get the latest NOS journaal (Dutch news) by send https://download.omroep.nl/nos/radionieuws/nosnieuws_bulalg.mp3
-            googlehome.play(message, function(notifyRes) {
-                console.log(message);
-            });
-        } else {
-            googlehome.notify(message, function(res) {
-                console.log(res);
-            });
-        }
+        handleMessage(message);
     });
     ws.send('Received');
 });
+
+app.get('/', function (req, res) {
+  res.send(req.querys);
+  if(req.query.message) {
+    handleMessage(req.query.message);
+  }
+});
+
+app.listen(PORT + 1, function () {
+  console.log('Example app listening on port ' + (PORT + 1));
+});
+
+function handleMessage(message) {
+   googlehome.ip('192.168.1.85', language);
+    if (message.startsWith('http')){
+        // @TODO: Add more checks.
+        // You can get the latest NOS journaal (Dutch news) by send https://download.omroep.nl/nos/radionieuws/nosnieuws_bulalg.mp3
+        googlehome.play(message, function(notifyRes) {
+            console.log(message);
+        });
+    } else {
+        googlehome.notify(message, function(res) {
+            console.log(res);
+        });
+    }
+}
